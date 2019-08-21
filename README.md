854af97b27177b0fdf1ce02794263dd85681532f
nanaones <nara03050@gmail.com>
2019-08-21 13:56:36 +0900

    add README, add sample SCORE

---
+++ README.md
@@ -0,0 +1,63 @@
+
+# SAMPLE SCORE for check return type
+
+#### requirement 
+```
+Docker
+chrome browser
+ICONex wallet
+```
+
+#### Step
+##### 1. start T-Bears docker container
+`$ docker run -it --name tbears -p 9000:9000 iconloop/tbears:mainnet`  
+##### 2. pull this repo[in docker container]
+`$ git pull https://github.com/nanaones/score-external-return-test`
+##### 3. deploy with T-Bears
+`$ tbears deploy -k keystore_test1 test`
+`Input your keystore password: `
+Keystore password is `test1_Account`
+then, you can see
+
+
+``
+Send deploy request successfully.
+If you want to check SCORE deployed successfully, execute txresult command
+transaction hash: 0xcd948ebdb5aa158b399f50963be87e921acdaabdd93a68481a535b038ac4b91e
+``
+**copy transaction hash**
+
+
+##### 4. check SCORE address
+   `$ tbears txresult [type txhash]`
+   like this
+   `$ tbears txresult 0xcd948ebdb5aa158b399f50963be87e921acdaabdd93a68481a535b038ac4b91e`
+
+##### 5. check it on ICONex(in chrome browser)
+   [how to change network](https://www.icondev.io/docs/how-to-change-network-in-iconex)
+
+
+---
+
+    ### Set network
+    ![img](/pic/icon.png)
+
+
+
+    ### Then, you can see your contract method on ICONex
+
+    #### move to contract tab & paste contract address.
+    ![img](/pic/contract.png)
+
+
+
+    #### you can see return values.
+    ##### Type bool
+    ![img](/pic/bool.png)
+    ##### Type dict
+    ![img](/pic/dict.png)
+    ##### Type int
+    ![img](/pic/int.png)
+    ##### Type list
+    ![img](/pic/list.png)
+    
---
+++ test/test.py
@@ -0,0 +1,94 @@
+from iconservice import *
+
+TAG = 'Test'
+
+class Test(IconScoreBase):
+
+    def __init__(self, db: IconScoreDatabase) -> None:
+        super().__init__(db)
+
+    def on_install(self) -> None:
+        super().on_install()
+
+    def on_update(self) -> None:
+        super().on_update()
+    
+    @external(readonly=True)
+    def str_hello(self) -> str:
+        Logger.debug(f'Hello, world!', TAG)
+        return "Hello, nanaones!"
+
+    @external(readonly=True)
+    def bool_hello(self) -> bool:
+        Logger.debug(f'Hello, world!', TAG)
+        return True
+
+    @external(readonly=True)
+    def bytes_hello(self) -> bytes:
+        Logger.debug(f'Hello, world!', TAG)
+        return bytes(b"Hello, nanaones!")
+
+    @external(readonly=True)
+    def int_hello(self) -> int:
+        Logger.debug(f'Hello, world!', TAG)
+        return 19920305
+
+    @external(readonly=True)
+    def dict_hello(self) -> dict:
+        Logger.debug(f'Hello, world!', TAG)
+        return {"name":"nanaones", "birth":19920305}
+
+    @external(readonly=True)
+    def list_hello(self) -> list:
+        Logger.debug(f'Hello, world!', TAG)
+        return ["Hello", "nanaones!", 19920305, True, bytes(b"Hello, nanaones!"), {"name":"nanaones", "birth":19920305}]
+
+# Not surported
+
+    # fail
+    # @external(readonly=True)
+    # def None_hello_return(self) -> None:
+    #     """
+    #     in deploy failure massage
+    #     {...
+    #         "status": "0x0",
+    #         "failure": {
+    #             "code": "0x5",
+    #             "message": "Unsupported type for 'None' at None_hello_return"
+    #     ...}
+    #     """
+    #     Logger.debug(f'Hello, world!', TAG)
+    #     return None
+
+    # # fail
+    # @external(readonly=True)
+    # def None_hello_no_return(self) -> None:
+    #     """
+    #     in deploy failure massage
+    #     ...
+    #     "status": "0x0",
+    #     "failure": {
+    #         "code": "0x5",
+    #         "message": "Unsupported type for 'None' at None_hello_no_return"
+    #     ...
+    #     """
+    #     Logger.debug(f'Hello, world!', TAG)
+
+    # fail
+    # @external(readonly=True)
+    # def None_hello_no_return_no_hint(self):
+    #     """
+    #     in deploy failure massage
+    #     ...
+    #         "status": "0x0",
+    #         "failure": {
+    #             "code": "0x5",
+    #             "message": "Returning type should be declared in read-only functions at None_hello_no_return_no_hint"
+    #         }
+    #     ...
+    #     """
+    #     Logger.debug(f'Hello, world!', TAG)
+
+
+    
+
